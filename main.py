adbe13d16974168e186562ed6fe91996b35d1b10
SNTz55 <101784360+SNTz55@users.noreply.github.com>
2024-02-17 21:45:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def home():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/time')
+def time():
+  return f"The time is: {datetime.datetime.utcnow}"
 
 if __name__ == '__main__':
   app.run()
