0dc93507596adb4a1926dbe178e790bd9a4f434c
SNTz55 <101784360+SNTz55@users.noreply.github.com>
2024-11-08 23:29:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
-import datetime
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+  return redirect("https://sntz55website.onrender.com/home")
+
+@app.route('/home')
 def home():
   return render_template('home.html')
 
